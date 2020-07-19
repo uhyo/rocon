92fb9a1aea4ac8bb2c6dd58d8eb9fbbd689c05c8
uhyo <uhyo@uhy.ooo>
2020-07-19 17:25:55 +0900

    feat: just add PathRoutesBuilder

---
+++ src/RoutesBuilder/PathRoutesBuilder/PathRoutesBuilder.test.ts
@@ -0,0 +1,168 @@
+import { PathRoutesBuilder } from ".";
+
+describe("PathRoutesBuilder", () => {
+  describe("routes", () => {
+    it("Empty at init", () => {
+      const b = PathRoutesBuilder.init();
+      expect(b.getRoutes()).toEqual({});
+    });
+
+    it("Reflects one routes() call", () => {
+      const res = PathRoutesBuilder.init<string>().routes({
+        foo: {
+          action: () => "foo!",
+        },
+        bar: {
+          action: () => "bar?",
+        },
+      });
+      const routes = res.getRoutes();
+      expect(Object.keys(routes)).toEqual(["foo", "bar"]);
+      expect(routes.foo.action()).toEqual("foo!");
+      expect(routes.foo.getLocation()).toEqual({
+        pathname: "/foo",
+        state: null,
+      });
+      expect(routes.bar.action()).toEqual("bar?");
+      expect(routes.bar.getLocation()).toEqual({
+        pathname: "/bar",
+        state: null,
+      });
+    });
+
+    it("Reflects two routes() calls", () => {
+      const res = PathRoutesBuilder.init<string>()
+        .routes({
+          foo: {
+            action: () => "foo!",
+          },
+        })
+        .routes({
+          bar: {
+            action: () => "bar?",
+          },
+        });
+      const routes = res.getRoutes();
+      expect(Object.keys(routes)).toEqual(["foo", "bar"]);
+      expect(routes.foo.action()).toEqual("foo!");
+      expect(routes.foo.getLocation()).toEqual({
+        pathname: "/foo",
+        state: null,
+      });
+      expect(routes.bar.action()).toEqual("bar?");
+      expect(routes.bar.getLocation()).toEqual({
+        pathname: "/bar",
+        state: null,
+      });
+    });
+
+    it("RoutesBuilder is immutable", () => {
+      const b1 = PathRoutesBuilder.init<string>().routes({
+        foo: {
+          action: () => "foo!",
+        },
+      });
+      const b2 = b1.routes({
+        bar: {
+          action: () => "bar?",
+        },
+      });
+      const routes1 = b1.getRoutes();
+      expect(Object.keys(routes1)).toEqual(["foo"]);
+      const routes2 = b2.getRoutes();
+      expect(Object.keys(routes2)).toEqual(["foo", "bar"]);
+    });
+
+    it("State handling", () => {
+      const res = PathRoutesBuilder.init<string>().routes({
+        foo: {
+          action: () => "foo!",
+        },
+        bar: {
+          action: (num: number) => `bar? ${num}`,
+        },
+      });
+      const routes = res.getRoutes();
+      expect(Object.keys(routes)).toEqual(["foo", "bar"]);
+      expect(routes.foo.action()).toEqual("foo!");
+      expect(routes.foo.getLocation()).toEqual({
+        pathname: "/foo",
+        state: null,
+      });
+      expect(routes.bar.action(123)).toEqual("bar? 123");
+      expect(routes.bar.getLocation()).toEqual({
+        pathname: "/bar",
+        state: null,
+      });
+    });
+  });
+
+  describe("attach", () => {
+    it("composed location action", () => {
+      const toplevel = PathRoutesBuilder.init<string>()
+        .routes({
+          foo: {
+            action: () => "foo!",
+          },
+        })
+        .getRoutes();
+      const sub = toplevel.foo
+        .attach(PathRoutesBuilder.init())
+        .routes({
+          bar: {
+            action: () => "bar!",
+          },
+        })
+        .getRoutes();
+
+      expect(sub.bar.action()).toBe("bar!");
+    });
+    it("composed location object", () => {
+      const toplevel = PathRoutesBuilder.init<string>()
+        .routes({
+          foo: {
+            action: () => "foo!",
+          },
+        })
+        .getRoutes();
+      const sub = toplevel.foo
+        .attach(PathRoutesBuilder.init())
+        .routes({
+          bar: {
+            action: () => "bar!",
+          },
+        })
+        .getRoutes();
+
+      expect(sub.bar.getLocation()).toEqual({
+        pathname: "/foo/bar",
+        state: null,
+      });
+    });
+    it("change location after attach", () => {
+      const sub = PathRoutesBuilder.init<string>().routes({
+        bom: {
+          action: () => "bom!",
+        },
+      });
+      const subRoutes = sub.getRoutes();
+      expect(subRoutes.bom.getLocation()).toEqual({
+        pathname: "/bom",
+        state: null,
+      });
+
+      const toplevel = PathRoutesBuilder.init<string>()
+        .routes({
+          foo: {
+            action: () => "foo!",
+          },
+        })
+        .getRoutes();
+      toplevel.foo.attach(sub);
+      expect(subRoutes.bom.getLocation()).toEqual({
+        pathname: "/foo/bom",
+        state: null,
+      });
+    });
+  });
+});
---
+++ src/RoutesBuilder/PathRoutesBuilder/index.ts
@@ -0,0 +1,28 @@
+import { RoutesBuilder, routesBuilderSpecies } from "..";
+import { PathLocationComposer } from "../../LocationComposer/PathLocationComposer";
+import { fillOptions } from "../fillOptions";
+import { RoutesBuilderOptions } from "../RoutesBuilderOptions";
+import { RoutesDefinition } from "../RoutesDefinitionObject";
+
+export type PathRoutesBuilderOptions = Omit<RoutesBuilderOptions, "composer">;
+
+/**
+ * Builder to define routes using pathname.
+ */
+export class PathRoutesBuilder<
+  ActionResult,
+  Defs extends RoutesDefinition<ActionResult>
+> extends RoutesBuilder<ActionResult, Defs> {
+  static init<ActionResult>(
+    options: Partial<PathRoutesBuilderOptions> = {}
+  ): PathRoutesBuilder<ActionResult, {}> {
+    const op = {
+      ...options,
+      composer: new PathLocationComposer(),
+    };
+    fillOptions(op);
+    return new PathRoutesBuilder(op);
+  }
+
+  readonly [routesBuilderSpecies] = PathRoutesBuilder;
+}
