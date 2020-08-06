64491b89223a7d18d1aa0077b5719ade8bfcbc5c
uhyo <uhyo@uhy.ooo>
2020-08-06 22:21:54 +0900

    refactor: remove wildcardRouteKey

--- src/builder/symbols.ts
+++ src/builder/symbols.ts
@@ -1,9 +1,4 @@
 /**
- * Name of the special route object field for wildcard route.
- */
-export const wildcardRouteKey = Symbol("wildcardRouteKey");
-
-/**
  * Name of the parent builder ot route record.
  */
 export const routeRecordParentKey = Symbol("routeRecordParentKey");
