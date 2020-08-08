826c88dc7d3718116387fdaae7833f4a1cf07a39
uhyo <uhyo@uhy.ooo>
2020-08-09 00:02:36 +0900

    feat: set up rocon/react export

--- src/react/Rocon.ts
+++ src/react/Rocon.ts
@@ -1,6 +1,12 @@
 export * as validator from "../validator";
+export { Link } from "./components/Link";
+export {} from "./components/RoconRoot";
 export {
   isLocationNotFoundError,
   LocationNotFoundError,
 } from "./errors/LocationNotFoundError";
+export { useHistory } from "./hooks/useHistory";
+export { useLocation } from "./hooks/useLocation";
+export { useNavigate } from "./hooks/useNavigate";
+export { useRoutes } from "./hooks/useRoutes";
 export * from "./shorthand";
