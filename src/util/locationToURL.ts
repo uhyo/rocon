e3569e0983181ed8b46ce8cf6939079442be16c2
uhyo <uhyo@uhy.ooo>
2020-08-10 15:41:24 +0900

    fix: remove double question mark

--- src/util/locationToURL.ts
+++ src/util/locationToURL.ts
@@ -1,7 +1,11 @@
 import { Location } from "../core/Location";
 
 export const locationToURL = (location: Location): string => {
-  return `${location.pathname}${location.search ? "?" + location.search : ""}${
-    location.hash ? "#" + location.hash : ""
-  }`;
+  return `${location.pathname}${
+    location.search
+      ? !location.search.startsWith("?")
+        ? "?" + location.search
+        : location.search
+      : ""
+  }${location.hash ? "#" + location.hash : ""}`;
 };
